5692101a334be2adeb8629e1e5a16e9a00304868
Junfeng Hu <junfeng.hu@screeningeagle.com>
2024-01-25 10:58:01 +0800

    Enable only RswiftLibrary and its target RswiftResources

--- Package.swift
+++ Package.swift
@@ -10,59 +10,59 @@ let package = Package(
         .watchOS(.v4),
     ],
     products: [
-        .executable(name: "rswift", targets: ["rswift"]),
+//        .executable(name: "rswift", targets: ["rswift"]),
         .library(name: "RswiftLibrary", targets: ["RswiftResources"]),
-        .plugin(name: "RswiftGenerateInternalResources", targets: ["RswiftGenerateInternalResources"]),
-        .plugin(name: "RswiftGeneratePublicResources", targets: ["RswiftGeneratePublicResources"]),
-        .plugin(name: "RswiftGenerateResourcesCommand", targets: ["RswiftGenerateResourcesCommand"]),
-        .plugin(name: "RswiftModifyXcodePackages", targets: ["RswiftModifyXcodePackages"]),
+//        .plugin(name: "RswiftGenerateInternalResources", targets: ["RswiftGenerateInternalResources"]),
+//        .plugin(name: "RswiftGeneratePublicResources", targets: ["RswiftGeneratePublicResources"]),
+//        .plugin(name: "RswiftGenerateResourcesCommand", targets: ["RswiftGenerateResourcesCommand"]),
+//        .plugin(name: "RswiftModifyXcodePackages", targets: ["RswiftModifyXcodePackages"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.11.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
+//        .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.11.1"),
+//        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
     ],
     targets: [
         .target(name: "RswiftResources"),
-        .target(name: "RswiftGenerators", dependencies: ["RswiftResources"]),
-        .target(name: "RswiftParsers", dependencies: ["RswiftResources", "XcodeEdit"]),
-
-        .testTarget(name: "RswiftGeneratorsTests", dependencies: ["RswiftGenerators"]),
-        .testTarget(name: "RswiftParsersTests", dependencies: ["RswiftParsers"]),
-
-        // Executable that brings all previous parts together
-        .executableTarget(name: "rswift", dependencies: [
-            .target(name: "RswiftParsers"),
-            .target(name: "RswiftGenerators"),
-            .product(name: "ArgumentParser", package: "swift-argument-parser"),
-        ]),
-
-        .plugin(name: "RswiftGenerateInternalResources", capability: .buildTool(), dependencies: ["rswift"]),
-        .plugin(name: "RswiftGeneratePublicResources", capability: .buildTool(), dependencies: ["rswift"]),
-        .plugin(
-            name: "RswiftGenerateResourcesCommand",
-            capability: .command(
-                intent: .custom(
-                    verb: "rswift-generate-resources",
-                    description: "Rswift generate resources"
-                ),
-                permissions: [
-                    .writeToPackageDirectory(reason: "Rswift generates a file with statically typed, autocompleted resources")
-                ]
-            ),
-            dependencies: ["rswift"]
-        ),
-        .plugin(
-            name: "RswiftModifyXcodePackages",
-            capability: .command(
-                intent: .custom(
-                    verb: "rswift-modify-xcode-packages",
-                    description: "Rswift modify Xcode packages"
-                ),
-                permissions: [
-                    .writeToPackageDirectory(reason: "Modifies Xcode project to fix package reference for plugins")
-                ]
-            ),
-            dependencies: ["rswift"]
-        ),
+//        .target(name: "RswiftGenerators", dependencies: ["RswiftResources"]),
+//        .target(name: "RswiftParsers", dependencies: ["RswiftResources", "XcodeEdit"]),
+//
+//        .testTarget(name: "RswiftGeneratorsTests", dependencies: ["RswiftGenerators"]),
+//        .testTarget(name: "RswiftParsersTests", dependencies: ["RswiftParsers"]),
+//
+//        // Executable that brings all previous parts together
+//        .executableTarget(name: "rswift", dependencies: [
+//            .target(name: "RswiftParsers"),
+//            .target(name: "RswiftGenerators"),
+//            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+//        ]),
+//
+//        .plugin(name: "RswiftGenerateInternalResources", capability: .buildTool(), dependencies: ["rswift"]),
+//        .plugin(name: "RswiftGeneratePublicResources", capability: .buildTool(), dependencies: ["rswift"]),
+//        .plugin(
+//            name: "RswiftGenerateResourcesCommand",
+//            capability: .command(
+//                intent: .custom(
+//                    verb: "rswift-generate-resources",
+//                    description: "Rswift generate resources"
+//                ),
+//                permissions: [
+//                    .writeToPackageDirectory(reason: "Rswift generates a file with statically typed, autocompleted resources")
+//                ]
+//            ),
+//            dependencies: ["rswift"]
+//        ),
+//        .plugin(
+//            name: "RswiftModifyXcodePackages",
+//            capability: .command(
+//                intent: .custom(
+//                    verb: "rswift-modify-xcode-packages",
+//                    description: "Rswift modify Xcode packages"
+//                ),
+//                permissions: [
+//                    .writeToPackageDirectory(reason: "Modifies Xcode project to fix package reference for plugins")
+//                ]
+//            ),
+//            dependencies: ["rswift"]
+//        ),
     ]
 )
